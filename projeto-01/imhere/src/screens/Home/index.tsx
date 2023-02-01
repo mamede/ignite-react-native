e676bc2932c64b7b540d460c93415a20475b320b
Felipe Mamede <felipebrasilmv@gmail.com>
2023-02-01 16:28:13 -0300

    feat: add project-01

---
+++ projeto-01/imhere/src/screens/Home/index.tsx
@@ -0,0 +1,55 @@
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+import { Participant } from "../../components/Participant";
+
+import { styles } from "./styles";
+
+export function Home() {
+  const participants = ['Mamede', 'Doe', 'Doe 2', 'Doe 3', 'Doe 4', 'Doe 5', 'Doe 6', 'Doe 7', 'Doe 8'];
+
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clicou em remover o participante ${name}`);
+  }
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {
+          participants.map(participant => (
+            <Participant 
+              key={participant} 
+              name={participant} 
+              onRemove={() => handleParticipantRemove("Mamede")} 
+            />
+          ))
+        }
+      </ScrollView>
+    </View>
+  )
+}
