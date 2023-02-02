b6d26a9c888205ff0ab23f56eb5389c9d0f9f572
Felipe Mamede <felipebrasilmv@gmail.com>
2023-02-02 12:58:32 -0300

    feat: add challenge 01

---
+++ projeto-01/todo-app/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import Home from "./src/screens/Home";
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
+  );
+}
