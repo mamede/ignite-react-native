268d8c8ed688ddbc8ccccb59a4b88ae3531217bb
Felipe Mamede <felipebrasilmv@gmail.com>
2023-06-20 19:33:16 -0300

    :art: utilizando mensagem de toast

--- projeto-03/ignitegym/src/screens/Profile/Profile.tsx
+++ projeto-03/ignitegym/src/screens/Profile/Profile.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading, useTheme } from 'native-base';
+
+// NATIVE BASE
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
+
+// EXPO
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 // COMPONENTS
 import { ScreenHeader } from '@components/ScreenHeader/ScreenHeader';
@@ -12,6 +18,45 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/mamede.png');
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelected(){
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if(photoSelected.canceled) {
+        return;
+      }
+
+      if(photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+        
+        if(photoInfo.exists && (photoInfo.size / 1024 / 1024 ) > 5){
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -29,12 +74,12 @@ export function Profile() {
             />
           :
             <UserPhoto 
-              source={{ uri: 'https://github.com/mamede.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           }
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar Foto
             </Text>
